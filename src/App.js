d221ceb794a85c19b92c2fe45cee95d5ce631e54
guestuser <guestuser@guestusers-MacBook-Air.local>
2020-07-29 22:43:48 -0400

    added all the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import ToDo from './ToDo'
 import './App.css';
+//import background from './IMG_3324.JPG'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+    <div className="App" >
+      <ToDo/>
+    </div>
     </div>
   );
 }
